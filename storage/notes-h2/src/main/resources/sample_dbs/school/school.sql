ae7fca868ac2770174f4dbd02b7a18686fda8586
詹子知 <zizhi.zhzzh@alibaba-inc.com>
2019-01-02 00:18:59 +0800

    Init School to H2 Database

---
+++ storage/notes-h2/src/main/resources/sample_dbs/school/school.sql
@@ -0,0 +1,51 @@
+BEGIN;
+
+DROP TABLE IF EXISTS teachers;
+CREATE TABLE teachers (
+    id serial PRIMARY KEY,
+    name varchar(32) NOT NULL,
+    bio text
+);
+
+DROP TABLE IF EXISTS classes;
+CREATE TABLE classes (
+    id serial PRIMARY KEY,
+    name varchar(32) NOT NULL UNIQUE,
+    department char(64) NOT NULL
+);
+
+DROP TABLE IF EXISTS courses;
+CREATE TABLE courses (
+    id serial PRIMARY KEY,
+    name varchar(32) NOT NULL,
+    period int default 60,
+    description text NOT NULL,
+    level smallint default 0,
+    status varchar(16) default 'AVAILABLE',
+    teacher_id int NOT NULL,
+    CONSTRAINT fk_course_teacher FOREIGN KEY(teacher_id) REFERENCES teachers(id)
+);
+
+DROP TABLE IF EXISTS students;
+CREATE TABLE students (
+    id serial PRIMARY KEY,
+    name varchar(32) NOT NULL,
+    first_name varchar(32),
+    last_name varchar(32),
+    class_id int,
+    bio text NOT NULL,
+    CONSTRAINT fk_student_class FOREIGN KEY(class_id) REFERENCES classes(id)
+);
+
+DROP TABLE IF EXISTS students_courses;
+CREATE TABLE students_courses (
+    student_id int NOT NULL,
+    course_id int NOT NULL,
+    score smallint,
+    choose_time timestamp default now() NOT NULL,
+    CONSTRAINT fk_student FOREIGN KEY(student_id) REFERENCES students(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT fk_course FOREIGN KEY(course_id) REFERENCES courses(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT uk_student_course UNIQUE(student_id, course_id)
+);
+
+COMMIT;
--- storage/notes-h2/src/test/java/me/jameszhan/notes/h2/H2HelloTest.java
+++ storage/notes-h2/src/test/java/me/jameszhan/notes/h2/H2HelloTest.java
@@ -1,9 +1,14 @@
 package me.jameszhan.notes.h2;
 
 import lombok.extern.slf4j.Slf4j;
+import org.h2.tools.RunScript;
 import org.h2.tools.Server;
 import org.junit.*;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.sql.*;
 
 /**
@@ -23,61 +28,107 @@ public class H2HelloTest {
     private String user = "sa";
     private String password = "";
 
-    @BeforeClass
-    public static void startServer() {
-        log.info("Start h2...");
-        try {
-            server = Server.createTcpServer().start();
-            log.info("h2 database start successful");
-        } catch (SQLException e) {
-            log.error("Start h2 error.", e);
-        }
-    }
-
-    @Before
-    public void setUp() throws ClassNotFoundException {
-        Class.forName("org.h2.Driver");
-    }
-
     @Test
-    public void testConnectionString() throws SQLException {
-        DriverManager.getConnection("jdbc:h2:~/h2_test");
-        DriverManager.getConnection("jdbc:h2:/Users/james/h2_test2");
-        DriverManager.getConnection("jdbc:h2:file:/Users/james/h2_test3");
-        DriverManager.getConnection("jdbc:h2:tcp://localhost/~/h2_test4");
-        DriverManager.getConnection("jdbc:h2:tcp://localhost:9092/~/h2_test5");
-    }
+    public void initDbSchool() throws SQLException {
+        String url = "jdbc:h2:mem:test_school";
 
-    @After
-    public void tearDown() {
-    }
-
-    @Test
-    public void useH2() throws Exception {
-        Connection conn = DriverManager.getConnection(dbDir, user, password);
-
-        Statement stat = conn.createStatement();
-        // insert data
-        stat.execute("CREATE TABLE TEST(NAME VARCHAR)");
-        stat.execute("INSERT INTO TEST VALUES('Hello World')");
-        // use data
-        ResultSet result = stat.executeQuery("select name from test ");
-        int i = 1;
-        while (result.next()) {
-            System.out.println(i++ + ":" + result.getString("name"));
+        String sqlPath = "sample_dbs/school/school.sql";
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        InputStream in = contextClassLoader.getResourceAsStream(sqlPath);
+        if (in == null) {
+            return;
         }
-        result.close();
-        stat.close();
-        conn.close();
-    }
 
-    @AfterClass
-    public static void stopServer() {
-        if (server != null) {
-            log.info("h2 status is {}", server.getStatus());
-            log.info("Stop h2...");
-            server.stop();
+        try (Connection conn = DriverManager.getConnection(url)) {
+            RunScript.execute(conn, new InputStreamReader(in, StandardCharsets.UTF_8));
+
+            URL teacherCsv = contextClassLoader.getResource("sample_dbs/school/teachers.csv");
+
+            try (Statement st = conn.createStatement()) {
+                if (teacherCsv != null) {
+                    st.executeUpdate("INSERT INTO teachers(name, bio) "
+                            + "SELECT name, bio FROM CSVREAD('" + teacherCsv + "')");
+                }
+            }
+
+//            insert into my_table( id, message, code ) values (
+//                    select convert( "id",bigint ), "message", convert( "code", bigint)
+//                    from CSVREAD( 'myfile.csv', 'id,message,code', null )
+//            );
+
+            try (Statement statement = conn.createStatement()) {
+                String sql = "SELECT * FROM teachers;";
+                try (ResultSet rs = statement.executeQuery(sql)) {
+                    ResultSetMetaData meta = rs.getMetaData();
+                    while (rs.next()) {
+                        for (int i = 0; i < meta.getColumnCount(); i++) {
+                            if (i == meta.getColumnCount() - 1) {
+                                log.info("{}: {}\n", meta.getColumnLabel(i + 1), rs.getString(i + 1));
+                            } else {
+                                log.info("{}: {}", meta.getColumnLabel(i + 1), rs.getString(i + 1));
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
+
+//    @BeforeClass
+//    public static void startServer() {
+//        log.info("Start h2...");
+//        try {
+//            server = Server.createTcpServer().start();
+//            log.info("h2 database start successful");
+//        } catch (SQLException e) {
+//            log.error("Start h2 error.", e);
+//        }
+//    }
+//
+//    @Before
+//    public void setUp() throws ClassNotFoundException {
+//        Class.forName("org.h2.Driver");
+//    }
+//
+//    @Test
+//    public void testConnectionString() throws SQLException {
+//        DriverManager.getConnection("jdbc:h2:~/h2_test");
+//        DriverManager.getConnection("jdbc:h2:/Users/james/h2_test2");
+//        DriverManager.getConnection("jdbc:h2:file:/Users/james/h2_test3");
+//        DriverManager.getConnection("jdbc:h2:tcp://localhost/~/h2_test4");
+//        DriverManager.getConnection("jdbc:h2:tcp://localhost:9092/~/h2_test5");
+//    }
+//
+//    @After
+//    public void tearDown() {
+//    }
+//
+//    @Test
+//    public void useH2() throws Exception {
+//        Connection conn = DriverManager.getConnection(dbDir, user, password);
+//
+//        Statement stat = conn.createStatement();
+//        // insert data
+//        stat.execute("CREATE TABLE TEST(NAME VARCHAR)");
+//        stat.execute("INSERT INTO TEST VALUES('Hello World')");
+//        // use data
+//        ResultSet result = stat.executeQuery("select name from test ");
+//        int i = 1;
+//        while (result.next()) {
+//            System.out.println(i++ + ":" + result.getString("name"));
+//        }
+//        result.close();
+//        stat.close();
+//        conn.close();
+//    }
+//
+//    @AfterClass
+//    public static void stopServer() {
+//        if (server != null) {
+//            log.info("h2 status is {}", server.getStatus());
+//            log.info("Stop h2...");
+//            server.stop();
+//        }
+//    }
 
 }
